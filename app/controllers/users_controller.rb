7db4fda5b7b2e31961be7065094dade611928e65
Chase Stephens & Elizabeth Hart & Karl Thomas & Kristen Moore <pair+chastep.elizabethhart.karlthomas925.kmoreo@devbootcamp.com>
2017-04-06 20:14:28 -0500

    log in/register taaaaabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
-  def new
+   def new
+    @user = User.new
   end
-  def create
-  end
+
   def show
+    return redirect_to new_user_path if !logged_in?
+    return redirect user_path if !authorized?(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
