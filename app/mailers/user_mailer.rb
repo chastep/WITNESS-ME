cee6dca506cb07a354df22ee7f335552bd6ccbb9
Elizabeth Hart <elizabethwhart@gmail.com>
2017-04-10 11:27:08 -0500

    Write welcome email method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://murmuring-caverns-54973.herokuapp.com/'
+    mail(to: @user.email, subject: 'Welcome to Witness Me')
+  end
 end
