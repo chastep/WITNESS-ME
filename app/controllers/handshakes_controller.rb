af819f0ead04fef52c80509a3c9a7c69898994ea
Karl Thomas <email": null>
2017-04-10 16:11:52 -0500

    added authentication to our routes ** made alot of lifestle tweaks so that our websockets and other branches of our website are more secure

--- app/controllers/handshakes_controller.rb
+++ app/controllers/handshakes_controller.rb
@@ -11,12 +11,23 @@ class HandshakesController < ApplicationController
   private
     def reroute?
       @challenge = Challenge.find(params[:id])
+      cookies.signed[:witness_id] = @challenge.witness_id
       @challenger = @challenge.challenger
       @acceptor = @challenge.acceptor
       return redirect_to root_path if !check_id(current_user)
+      return redirect_to user_path(current_user) if have_already_shaken?(shake_count)
     end
 
     def check_id(user)
       return (user == @challenge.challenger || user == @challenge.witness || user == @challenge.acceptor)
+    end
+
+    def shake_count
+      shakes = Handshake.where(challenge_id: @challenge.id).count
+      return shakes
+    end
+
+    def have_already_shaken?(count)
+      return count >= 2
     end
 end
