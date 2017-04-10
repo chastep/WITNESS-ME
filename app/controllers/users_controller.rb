3acddb7ef2c1ec405de78b21eb36c5ce69a34111
Chase Stephens & Kristen Moore <pair+chastep.kmoreo@devbootcamp.com>
2017-04-09 21:42:16 -0500

    update validations and user profile pic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.split_name(params[:user][:full_name])
     # create dwolla customer account and id
-    # app_token = $dwolla.auths.client
     request_body = @user.customer_request_body
     customer = $app_token.post "customers", request_body
     @user.dwolla_url = customer.headers[:location]
-    # users = app_token.get "customers"
     @user.dwolla_id = $users._embedded.customers[0].id
     if @user.save
       session[:user_id] = @user.id
@@ -32,7 +30,6 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find_by(id: params[:id])
-    # app_token = $dwolla.auths.client
     customer_url = @user.dwolla_url
     customer = $app_token.post "#{customer_url}/iav-token"
     @token = customer.token
@@ -43,25 +40,13 @@ class UsersController < ApplicationController
     if request.xhr?
       res = params[:user][:dwolla_url][:_links][:"funding-source"][:href]
       @user.update_attributes(dwolla_url: res)
-
       @user.dwolla_url
-      # p "-----------------------------------------"
-      # p @user
-      # p "-----------------------------------------"
-      # app_token = $dwolla.auths.client
-      # root = app_token.get "/"
-      # bucket_location = root._links.account.href
-      # bucket = $app_token.get "#{bucket_location}/funding-sources"
-      # bucket_url = bucket._embedded[:"funding-sources"][0][:"_links"][:"self"][:"href"]
-      # p $bucket_url
-      # p "-----------------------------------------"
     end
-    # redirect_to "/users/<%= current_user.id %>"
   end
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :phone, :username, :dwolla_url)
+      params.require(:user).permit(:name, :email, :password, :phone, :username, :dwolla_url, :profile_picture)
     end
 
 end
