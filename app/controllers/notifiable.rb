2961692d4ed874ea8a98f43e670dd18d729b2b8c
Chase Stephens & Kristen Moore <pair+chastep.kmoreo@devbootcamp.com>
2017-04-11 15:42:55 -0500

    witness only working, functions built, looking at dwolla funding sources now

--- app/controllers/notifiable.rb
+++ app/controllers/notifiable.rb
@@ -3,9 +3,9 @@ module Notifiable
   def notify(challenge, path)
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
 
-    message_1 = client.messages.create from: '980-272-5310', to: "#{challenge.acceptor.phone}", body: "#{challenge.challenger.username} has challenged you to the following: #{challenge.description}. Go to WITNESS-ME to play! http://murmuring-caverns-54973.herokuapp.com/#{path}"
+    message_1 = client.messages.create from: '980-272-5310', to: "#{challenge.acceptor.phone}", body: "#{challenge.challenger.username.upcase} has challenged you to the following: #{challenge.description}. Go to WITNESS-ME to play! http://murmuring-caverns-54973.herokuapp.com"
 
-    message_2 = client.messages.create from: '980-272-5310', to: "#{challenge.witness.phone}", body: "#{challenge.challenger.username} has invited you to witness the following: #{challenge.description}. Go to WITNESS-ME to be a witness! http://murmuring-caverns-54973.herokuapp.com/#{path}"
+    message_2 = client.messages.create from: '980-272-5310', to: "#{challenge.witness.phone}", body: "#{challenge.challenger.username.upcase} has invited you to witness the following: #{challenge.description}. Go to WITNESS-ME to be a witness! http://murmuring-caverns-54973.herokuapp.com"
   end
 
 end
