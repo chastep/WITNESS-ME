7333ef4049df718bae35e3f5aeddce6ec6e03821
Chase Stephens & Kristen Moore <pair+chastep.kmoreo@devbootcamp.com>
2017-04-11 10:17:59 -0500

    correct links for pending challenges to handshake page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,10 +20,15 @@ $(document).on('turbolinks:load', function () {
   $('.challenge-content').on('click', function(e) {
     e.preventDefault();
     var listItem = $(this).closest('li');
-    console.log(listItem);
     var challengeID = listItem.attr("id");
-    console.log(challengeID);
     location.replace("/challenges/" + challengeID);
+  });
+
+  $('.pending-challenge').on('click', function(e) {
+    e.preventDefault();
+    var listItem = $(this).closest('li');
+    var challengeID = listItem.attr("id");
+    location.replace("/handshakes/" + challengeID);
   });
 
   $('#challenge_completed').on('click', function(e) {
