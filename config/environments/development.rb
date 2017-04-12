c87e7a04fe5f1bcd7dc059e1b826f8bfa87513a6
Chase Stephens <email": null>
2017-04-12 12:32:04 -0500

    add aws keys/info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,16 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
