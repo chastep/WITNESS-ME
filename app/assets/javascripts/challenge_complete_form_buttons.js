c98e59b9ca055c6c8f31e916570b80350bd2d837
Chase Stephens & Kristen Moore <pair+chastep.kmoreo@devbootcamp.com>
2017-04-11 21:25:31 -0500

    tweaks

--- app/assets/javascripts/challenge_complete_form_buttons.js
+++ app/assets/javascripts/challenge_complete_form_buttons.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
-  $('#challenge_completed').on('click', function(e) {
-    debugger
-    e.preventDefault();
-    $('#challenge_participants_buttons').removeClass('hide');
-    $(this).addClass('hide');
-  });
+  // $('#challenge_completed').on('click', function(e) {
+  //   debugger
+  //   e.preventDefault();
+  //   $('#challenge_participants_buttons').removeClass('hide');
+  //   $(this).addClass('hide');
+  // });
 
-  $('#challenge_participants_buttons').on('submit', 'form', function(e) {
-    e.preventDefault();
-    debugger
-    var divItem = $(this).closest('div');
-    var winnerID = divItem.attr("id");
-    var ajaxFunc = $.ajax({
-        url: "<%= challenge_path(@challenge) %>",
-        method: 'PUT',
-        dataType: 'json',
-        data: {user: {winner_id: winnerID}}
-      });
+  // $('#challenge_participants_buttons').on('submit', 'form', function(e) {
+  //   e.preventDefault();
+  //   debugger
+  //   var divItem = $(this).closest('div');
+  //   var winnerID = divItem.attr("id");
+  //   var ajaxFunc = $.ajax({
+  //       url: "<%= challenge_path(@challenge) %>",
+  //       method: 'PUT',
+  //       dataType: 'json',
+  //       data: {user: {winner_id: winnerID}}
+  //     });
 
-      ajaxFunc.done(function(response) {
-        console.log(response);
-        setTimeout(
-          function() {
-            location.replace("<%= challenge_path(@challenge) %>");
-          }, 3000);
-      });
-      ajaxFunc.fail(function(response) {
-        console.log("shits broke fam");
-      });
-  });
+  //     ajaxFunc.done(function(response) {
+  //       console.log(response);
+  //       setTimeout(
+  //         function() {
+  //           location.replace("<%= challenge_path(@challenge) %>");
+  //         }, 3000);
+  //     });
+  //     ajaxFunc.fail(function(response) {
+  //       console.log("shits broke fam");
+  //     });
+  // });
 
 });
